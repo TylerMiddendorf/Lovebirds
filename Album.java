b415be0edde4a5e7de58fdbbeb8796cc7ac84ada
iangowland22 <110187447+iangowland22@users.noreply.github.com>
2024-03-10 21:52:50 -0500

    upload Album and Photo

---
+++ Album.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Album {
+
+    private String albumName;
+    private int photoCount;
+    private ArrayList<Photo> album;
+
+    public Album(String albumName, ArrayList<Photo> album){
+        this.albumName = albumName;
+        this.photoCount = album.size();
+        this.album = album;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public String getAlbumName() {
+        return this.albumName;
+    }
+
+    public void addPhoto(Photo toAdd) {
+        this.album.add(toAdd);
+        this.photoCount++;
+    }
+
+    public Photo getPhoto(String photoID) {
+        int i = 0;
+        boolean isFound = false;
+        Photo found = null;
+        while(i < album.size() && !isFound) {
+            String currentID = album.get(i).getPhotoID();
+            if(currentID.equals(photoID)) {
+                found = album.get(i);
+                isFound = true;
+            }
+        }
+        if(!isFound) {
+            // the Photo variable found is still set to null and will return null
+            System.out.println("Photo not found with that ID.");
+        } 
+        return found;
+    }
+    
+}
