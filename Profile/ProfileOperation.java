9fd9209ed40dcb29c46552f76f9ae5c032fc338b
Jacob <jacobspaide@gmail.com>
2024-04-03 22:43:53 -0500

    added methods to ProfileOperation

---
+++ Profile/ProfileOperation.java
@@ -0,0 +1,22 @@
+public class ProfileOperation{
+
+    public Photo createPhoto()
+    {
+        return null;
+    }
+
+    public Photo readPhoto()
+    {
+        return null;
+    }
+
+    public boolean updatePhoto()
+    {
+        return false;
+    }
+
+    public boolean deleteAlbum()
+    {
+        return false;
+    }
+}
