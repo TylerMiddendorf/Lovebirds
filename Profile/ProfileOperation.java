0739bf6dc035fa7d36748ef60c43b0b161375c81
iangowland22 <iangowland22@gmail.com>
2024-04-03 23:07:16 -0500

    last handler and operation file

---
+++ Profile/ProfileOperation.java
@@ -0,0 +1,20 @@
+public class ProfileOperation {
+
+    public void createProfile() {
+        //
+    }
+
+    public void readProfile() {
+        //
+    }
+
+    public boolean updateProfile() {
+        return true;
+    }
+
+    public boolean deleteProfile() {
+        return true;
+    }
+
+
+}
