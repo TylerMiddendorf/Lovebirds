cf14f4c382a1618394d39ad89def1e9355e1365b
Jacob <jacobspaide@gmail.com>
2024-04-18 17:01:23 -0500

    adding to controller

--- Controller.java
+++ Controller.java
@@ -1,12 +1,22 @@
+import java.util.ArrayList;
 
 class Controller{
-    public static void main(String...args){
-        
+    
+    private AlbumHandler albumHandler;
+    private MatchmakerHandler matchmakerHandler;
+    private ProfileHandler profileHandler;
+    private ChatHandler chatHandler;
+    private PhotoHandler photoHandler;
+    
 
-
-
-
+    /*
+     * calls retrieveSugUsers from Matchmaker handler
+     * @param id
+     * @return suggested
+     */
+    public ArrayList<Profile> retrieveSugUsers(int id){
+        ArrayList<Profile> suggested = matchmakerHandler.retrieveSugUsers(id);
+        return suggested;
     }
-
 
 }
