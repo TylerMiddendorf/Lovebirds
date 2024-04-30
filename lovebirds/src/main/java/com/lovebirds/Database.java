a72b80afaf1f67ca646e56ec153d9b2f1d338f47
iangowland22 <iangowland22@gmail.com>
2024-04-30 18:10:55 -0500

    trying to make database work

--- lovebirds/src/main/java/com/lovebirds/Database.java
+++ lovebirds/src/main/java/com/lovebirds/Database.java
@@ -1,8 +1,13 @@
 package lovebirds.src.main.java.com.lovebirds;
 
+import java.sql.Connection;
+
 public abstract class Database {
     
     private String username;
     private String password;
-    public abstract Database connect();
+    private Connection conn;
+    public abstract boolean connect();
+    public abstract Connection getConnection();
+
 }
--- lovebirds/src/main/java/com/lovebirds/MySQLDatabase.java
+++ lovebirds/src/main/java/com/lovebirds/MySQLDatabase.java
@@ -1,17 +1,49 @@
 package lovebirds.src.main.java.com.lovebirds;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MySQLDatabase extends Database {
 
-    
-    public MySQLDatabase(Database db) {
-        
+    private String username;
+    private String password;
+    private Connection conn;
 
+    public MySQLDatabase() {
+        username = "root";
+        conn = null;
     }
 
-    public Database connect() {
+    public boolean connect(String password) {
 
-        return this;
+        String path = "jdbc:mysql://localhost:3306/lovebirds_schema";
+
+        try {
+            conn = DriverManager.getConnection(path, username, password);
+            return true;
+        } catch (SQLException e) {
+           
+            System.out.println(e.getSQLState());
+            System.out.println(e.getMessage());
+            //System.out.println("Could not connect to database.");
+            return false;
+        }
+    
+    }  
+
+    public Connection getConnection() {
+        return conn;
+    }
+
+    public String getUsername() {
+        return username;
     }  
     
+    public static void main(String[] args) {
+        MySQLDatabase db = new MySQLDatabase();
+        System.out.println(db.connect("Root123!@#"));
+    }
 }
 
