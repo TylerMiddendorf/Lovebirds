b4ba4a7cdbd748067a3277133762222fe82798ed
Tyler Middendorf <60022505+TylerMiddendorf@users.noreply.github.com>
2024-05-01 14:47:57 -0500

    updated profile OP parameters

--- lovebirds/src/main/java/com/lovebirds/ProfileOperation.java
+++ lovebirds/src/main/java/com/lovebirds/ProfileOperation.java
@@ -5,10 +5,10 @@ public abstract class ProfileOperation {
     public abstract Profile createProfile(Profile profile);
     public abstract Profile readProfile(int userID);
     public abstract boolean updateProfile(Profile profile);
-    public abstract boolean deleteProfile();
-    public abstract Preferences createPreferences();
-    public abstract Preferences readPreferences();
-    public abstract boolean updatePreferences();
-    public abstract boolean deletePreferences();
+    public abstract boolean deleteProfile(int userID);
+    public abstract Preferences createPreferences(int userID, Preferences newPref);
+    public abstract Preferences readPreferences(int userID);
+    public abstract boolean updatePreferences(int userID, Preferences newPref);
+    public abstract boolean deletePreferences(int userID);
 
 }
