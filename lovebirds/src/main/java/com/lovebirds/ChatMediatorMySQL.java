6808de6360c50c3f48b52d5070c7446e8b58c1a4
iangowland22 <iangowland22@gmail.com>
2024-05-01 20:52:19 -0500

    last commit for the day

--- lovebirds/src/main/java/com/lovebirds/ChatMediatorMySQL.java
+++ lovebirds/src/main/java/com/lovebirds/ChatMediatorMySQL.java
@@ -1,5 +1,11 @@
 package com.lovebirds;
 
+import java.sql.Connection;
+import java.util.Date;
+import java.sql.PreparedStatement;
+import java.time.LocalDateTime;
+
+
 public class ChatMediatorMySQL extends ChatMediator{ // 
 
 
@@ -10,7 +16,17 @@ public class ChatMediatorMySQL extends ChatMediator{ //
     }
     
     public Chat createChat(Chat chat) {
-        return new Chat();
+        try {
+            db.connect();
+            Connection dbConn = db.getConnection();
+            String sql = "INSERT INTO PREFERENCES(TIMESTAMP, SENDER, RECIPIENT, MESSAGE) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = dbConn.prepareStatement(sql);
+            Date date = new Date();
+            
+            return new Chat();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public Chat readChat() {
