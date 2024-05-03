2de90925a49a5ca24b4e956232d73d7f73b878dd
marchernando <marcohernandez0531@gmail.com>
2024-05-02 23:39:08 -0500

    removed unnecessary sql imports in ProfileHandler

--- lovebirds/src/main/java/com/lovebirds/ProfileHandler.java
+++ lovebirds/src/main/java/com/lovebirds/ProfileHandler.java
@@ -1,10 +1,6 @@
 package com.lovebirds;
 
 import java.util.Scanner;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class ProfileHandler extends Handler {
 
