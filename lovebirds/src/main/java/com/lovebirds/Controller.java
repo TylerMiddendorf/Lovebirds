865e4f00369bc85e4bfa70d52c5d956ec29f3e4f
EttyYeti <dromo2327@gmail.com>
2024-05-05 18:45:21 -0500

    I FIXED DISPLAY SUGGESED USERS / MATCHES RAHHHHHHHHHHHHHHHHHH

--- lovebirds/src/main/java/com/lovebirds/Controller.java
+++ lovebirds/src/main/java/com/lovebirds/Controller.java
@@ -22,7 +22,7 @@ public class Controller {
      * @return suggestedUsers
      */
     public ArrayList<Profile> retrieveSugUsers() {
-        ArrayList<Profile> suggestedUsers = null; //matchmakerHandler.retrieveSugUsers();
+        ArrayList<Profile> suggestedUsers = matchmakerHandler.retrieveSugUsers();
         return suggestedUsers;
     }
 
