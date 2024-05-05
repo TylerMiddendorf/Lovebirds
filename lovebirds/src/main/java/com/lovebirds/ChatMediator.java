6a40bd344d109823a26588d3b4546d4bbc08a009
Tyler Middendorf <60022505+TylerMiddendorf@users.noreply.github.com>
2024-05-05 15:14:52 -0500

    Added getProfile functions

--- lovebirds/src/main/java/com/lovebirds/ChatMediator.java
+++ lovebirds/src/main/java/com/lovebirds/ChatMediator.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 
 @SuppressWarnings("unused") public  abstract class ChatMediator {
 
-     
+    
     private Database db;
     public abstract boolean createChat(String message, int SenderID, int RecieverID);
     public abstract Chat readChat(int senderID, int recieverID);
     public abstract boolean deleteChat(int senderID, int recieverID);
     public abstract ArrayList<Integer> readMatches(int userID);
+    public abstract Profile readProfile(int userID);
     
 }
