c6a5c7bd8851abf0f4027ff760439db18fe7f5d4
Tyler Middendorf <60022505+TylerMiddendorf@users.noreply.github.com>
2024-05-05 23:58:29 -0500

    updated

---
+++ lovebirds/test/MediaOperationsTest.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.lovebirds.Authentication;
+import com.lovebirds.FactoryProducer;
+import com.lovebirds.MediaOperationMySQL;
+import com.lovebirds.Profile;
+import com.lovebirds.Preferences;
+import com.lovebirds.ProfileOperation;
+
+
+public class MediaOperationsTest {
+    @Test
+    public void testCreateProfile() {
+        MediaOperation MediaOperationOperation = FactoryProducer.getSQLFactory().getMedia("MySQL");
+        int profileID = 1;
+        String path = "test.png"; 
+        String album = "pizza";
+        String name = "test";
+        int uid = 2;
+        boolean created = mediaOperation.createPhoto(test.png, album, name,uid);
+        Assert.assertEquals(true, created);
+    }
+}
